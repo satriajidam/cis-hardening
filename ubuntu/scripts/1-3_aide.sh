3d826cc61a6586febd55f4d6748e8352368b8a50
Agastyo Satriaji Idam <play.satriajidam@gmail.com>
2018-08-20 17:01:08 +0700

    Add AIDE config script

--- ubuntu/scripts/1-3_aide.sh
+++ ubuntu/scripts/1-3_aide.sh
@@ -20,16 +20,17 @@ fi
 # ref: https://www.cyberciti.biz/faq/debian-ubuntu-linux-software-integrity-checking-with-aide
 #      https://www.stephenrlang.com/2016/03/using-aide-for-file-integrity-monitoring-fim-on-ubuntu
 
-begin_msg "Installing AIDE..."
+begin_msg 'Installing AIDE...'
 
 echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
 echo "postfix postfix/mailname string $(hostname -f)" | debconf-set-selections
 
 apt-get install -y aide aide-common
 
-success_msg "AIDE installed!"
+success_msg 'AIDE installed!'
 
-begin_msg "Configuring AIDE..."
+
+begin_msg 'Configuring AIDE...'
 
 grep -R -E '^!/var/lib/lxcfs/cgroup$' /etc/aide/* > /dev/null 2>&1 && err=$? || err=$?
 if [ $err -ne 0 ]; then
@@ -43,9 +44,10 @@ fi
 
 aideinit --yes --force
 
-success_msg "AIDE configured!"
+success_msg 'AIDE configured!'
 
-begin_msg "Enabling daily AIDE check..."
+
+begin_msg 'Enabling daily AIDE check...'
 
 cp -vf $(get_config_dir)/aidecheck.service /etc/systemd/system/aidecheck.service
 cp -vf $(get_config_dir)/aidecheck.timer /etc/systemd/system/aidecheck.timer
@@ -57,4 +59,4 @@ systemctl restart aidecheck.timer
 systemctl daemon-reload
 systemctl status aidecheck.timer --no-pager
 
-success_msg "Daily AIDE check enabled!"
+success_msg 'Daily AIDE check enabled!'
