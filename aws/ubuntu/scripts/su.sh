6b0dcc92947cdfbf4de928378ab9ac2a01afc8f5
Agastyo Satriaji Idam <play.satriajidam@gmail.com>
2018-08-24 10:21:21 +0700

    Add su config script

---
+++ aws/ubuntu/scripts/su.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+########################################################################
+# Script  : Configure su access.
+# OSs     : - Ubuntu 16.04
+# Authors : - Agastyo Satriaji Idam (play.satriajidam@gmail.com)
+########################################################################
+
+set -o errexit # make script exits when a command fails
+set -o pipefail # make script exits when the rightmost command of a pipeline exits with non-zero status
+set -o nounset # make script exits when it tries to use undeclared variables
+# set -o xtrace # trace what gets executed for debugging purpose
+
+# ensure running as root
+if [ "$(id -u)" != "0" ]; then
+  exec sudo "$0" "$@"
+  exit $?
+fi
+
+begin_msg 'Limiting su access...'
+
+dpkg-statoverride --update --add root "$SU_GROUP" 4750 /bin/su
+
+success_msg 'su access limited!'
