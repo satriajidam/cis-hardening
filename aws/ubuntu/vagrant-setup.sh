0ebbe8798a8b39ddc26f924bc11c66958e3ab0c7
Agastyo Satriaji Idam <play.satriajidam@gmail.com>
2018-08-26 17:40:56 +0700

    Use git repository to prepare provisioner scripts

--- aws/ubuntu/vagrant-setup.sh
+++ aws/ubuntu/vagrant-setup.sh
@@ -5,3 +5,7 @@ sed -i "/mesg n/d" /root/.profile
 
 # add vagrant user to vagrant group
 usermod -aG vagrant vagrant
+
+cd /home/vagrant && \
+  git clone https://github.com/satriajidam/cis-hardening.git && \
+  cis-hardening/aws/ubuntu/scripts/run.sh
